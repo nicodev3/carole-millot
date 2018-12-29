e4368962bbca4b23e564bcb4c3e007b0669f2af5
nicodev <ndevaux@gmail.com>
2018-12-29 18:59:48 +0100

    mobile nav

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,7 @@
 // JS Goes here - ES6 supported
+const hamburgerBox = document.querySelector('.hamburger-box');
+const menu = document.querySelector('.g-header__nav');
 
+hamburgerBox.addEventListener('click', function() {
+  menu.classList.toggle('active');
+}, false);
